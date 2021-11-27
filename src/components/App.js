ab23593426581df671635faeb8125420f6863b36
Abbigale Bercher <abbeybercher@gmail.com>
2021-11-27 12:23:44 -0500

    commit tree b6daf85f07cdb11f872f59a5baf9fd34bc182329

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
 // create your App component here
+import React, { useEffect, useState } from 'react'
+
+function App(){
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [dogImage, setDogImage] = useState(null)
+
+        useEffect(() => {
+            fetch('https://dog.ceo/api/breeds/image/random')
+            .then(response => response.json()
+            .then(data => {
+                setDogImage(data.message);
+                setIsLoaded(true);
+            }))
+        }, [])
+
+        if (!isLoaded) return <p>Loading...</p>
+
+    return (
+        <div>
+            <img src={dogImage} alt="A Random Dog"></img>
+        </div>
+    )
+}
+
+export default App
